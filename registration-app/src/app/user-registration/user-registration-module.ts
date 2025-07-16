7aa66b579bb3eaab314febc0d7a04af1aa7cb4e7
pushpinder singh <pushpinder.1990singh@gmail.com>
2025-07-16 19:56:12 -0400

    Header Component

---
+++ registration-app/src/app/user-registration/user-registration-module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserRegistration } from './user-registration';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    UserRegistration
+  ]
+})
+export class UserRegistrationModule { }
---
+++ registration-app/src/app/user-registration/user-registration.ts
@@ -0,0 +1,61 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-registration',
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './user-registration.html',
+  styleUrl: './user-registration.scss'
+})
+export class UserRegistration {
+  registerForm!: FormGroup;
+  submittedData: any = {};
+
+  constructor(private fb : FormBuilder){
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email:['', [Validators.required]],
+      phoneNumber:['',[Validators.required]],
+      address : this.fb.group({
+              street:['',Validators.required],
+              city:['',Validators.required]
+              }),
+      phoneNumbers: this.fb.array([
+        this.fb.control('',[Validators.required]),
+      ]), 
+    });
+  }
+
+  //Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+  //Validators.pattern(/^[0-9]{10}$/)
+
+  get phoneNumbers(){
+    return this.registerForm.get('phoneNumbers') as FormArray;
+  }
+
+  addPhone(){
+    this.phoneNumbers.push(
+      this.fb.control('',[Validators.required])
+    )
+  }
+
+  removePhone(index: number){
+      this.phoneNumbers.removeAt(index);
+  }
+
+  submitForm(){
+    console.log('Register Form :',this.registerForm.value)
+    
+    // if(this.registerForm.valid){
+      this.submittedData = this.registerForm.value;
+      const street = this.submittedData.address.street;
+      const city = this.submittedData.address.city;
+    // }
+  }
+
+}
